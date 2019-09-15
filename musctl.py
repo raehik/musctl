8467dde37ab7b27fad6e2603441e14046a1f4e1b
Ben Orchard <thefirstmuffinman@gmail.com>
2019-09-15 21:19:48 +0100

    separate from raehutils (needs to be standalone)

--- musctl.py
+++ musctl.py
@@ -3,14 +3,85 @@
 # Manage and maintain a music library.
 #
 
-import raehutils
-import sys, os, argparse, logging
-
+import sys, os, argparse, logging, subprocess
 import configparser
-
 import hashlib
 
-class MusCtl(raehutils.RaehBaseClass):
+def get_shell(cmd, cwd=None):
+    """Run a shell command, blocking execution, detaching stdin, stdout and
+    stderr.
+
+    Useful for grabbing shell command outputs, or if you want to run something
+    silently and wait for it to finish.
+
+    @param cmd command to run as an array, where each element is an argument
+    @param cwd if present, directory to use as CWD
+    @return the command's return code, stdout and stderr (respectively, as a
+            tuple)
+    """
+    proc = subprocess.run(cmd, stdin=subprocess.DEVNULL,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               cwd=cwd)
+    return proc.returncode, \
+           proc.stdout.decode("utf-8", "replace").strip(), \
+           proc.stderr.decode("utf-8", "replace").strip()
+
+def drop_to_shell(cmd, cwd=None):
+    """Run a shell command, blocking execution.
+
+    Doesn't touch any pipes. Like dropping to shell during execution.
+
+    @param cmd command to run as an array, where each element is an argument
+    @param cwd if present, directory to use as CWD
+    @return the command's exit code
+    """
+    return subprocess.run(cmd, cwd=cwd).returncode
+
+class BaseClass:
+    def _deinit(self):
+        self.logger.debug("deinitialising...")
+
+    ## CLI-related {{{
+    def _init_logging(self):
+        self.logger = logging.getLogger(os.path.basename(sys.argv[0]))
+        lh = logging.StreamHandler()
+        lh.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
+        self.logger.addHandler(lh)
+
+    def _parse_verbosity(self):
+        if self.args.verbose == 1:
+            self.logger.setLevel(logging.INFO)
+        elif self.args.verbose >= 2:
+            self.logger.setLevel(logging.DEBUG)
+        if self.args.quiet >= 1:
+            # reset verbosity (to make verbose/quiet checks easier)
+            self.args.verbose = 0
+            self.logger.setLevel(logging.NOTSET)
+
+    def run(self):
+        """Run from CLI: parse arguments, run main, deinitialise."""
+        self._init_logging()
+        self._parse_args()
+        self.main()
+        self._deinit()
+    ## }}}
+
+    def fail(self, msg="internal error", ret=1):
+        """Exit with a message and a return code.
+
+        Should only be used for errors -- if you want to deinitialise and exit
+        safely, simply return from main.
+
+        Suggested to use no parameters for internal functions that the user
+        doesn't need to know about (which generally indicates a logic error,
+        rather than an input one.)
+        """
+        self.logger.error(msg)
+        self._deinit()
+        sys.exit(ret)
+
+class MusCtl(BaseClass):
     DEF_CONFIG_FILE = os.path.join(os.environ.get("XDG_CONFIG_HOME") or os.path.expandvars("$HOME/.config"), "musctl", "config.ini")
 
     DEF_CONVERT_EXTS = ["flac"]
@@ -338,7 +409,7 @@ class MusCtl(raehutils.RaehBaseClass):
         @return the track's stored hash
         """
         cmd_ffprobe_hash = ["ffprobe", track, "-show_entries", "stream_tags={}".format(MusCtl.HASH_FIELD), "-of", "csv=p=0"]
-        rc, stdout, _ = raehutils.get_shell(cmd_ffprobe_hash)
+        rc, stdout, _ = get_shell(cmd_ffprobe_hash)
         if rc != 0:
             self.fail("error while retrieving stored hash: {}".format(track), MusCtl.ERR_FFMPEG)
         retrieved_hash = stdout.strip()
@@ -359,9 +430,9 @@ class MusCtl(raehutils.RaehBaseClass):
         """
         rc = 0
         if self.args.quiet == 0 and self.args.verbose >= min_verb_lvl:
-            rc = raehutils.drop_to_shell(cmd, cwd=cwd)
+            rc = drop_to_shell(cmd, cwd=cwd)
         else:
-            rc = raehutils.get_shell(cmd, cwd=cwd)[0]
+            rc = get_shell(cmd, cwd=cwd)[0]
         return rc
 
 if __name__ == "__main__":
